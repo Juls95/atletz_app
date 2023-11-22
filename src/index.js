59acc46eea8a13fc0edae17f527fc62e7d32acba
Juls95 <jurami89@hotmail.com>
2023-11-22 16:35:38 -0600

    "adding basic files"

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import styled from 'styled-components';
+import styles from './MyComponent.module.css';
+import './styles.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
